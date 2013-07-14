7650aa67cd153e6213e78868e6d6c2fafaa8dce4
Sean Silva <silvas@purdue.edu>
2013-07-14 16:50:02 -0700

    Remove spurious space.

--- CompilationDatabaseMagic.h
+++ CompilationDatabaseMagic.h
@@ -1,6 +1,6 @@
 // The following macros should be defined on entry to this file:
 // __COMPDB_ENTRY:
-//   The contents of this compilation database entry.  Should be a string
+//   The contents of this compilation database entry. Should be a string
 //   literal.
 // __COMPDB_SYMNAME:
 //   ATM I can't think of a way to generate a unique symbol name, so just
