e0293c2a6359fa7e3e904322bd0f5112b536695c
Sean Silva <silvas@purdue.edu>
2013-07-14 01:56:52 -0700

    This is really a "clang" feature, if anything.

--- CompilationDatabaseMagic.h
+++ CompilationDatabaseMagic.h
@@ -5,5 +5,5 @@
 // __COMPDB_SYMNAME:
 //   ATM I can't think of a way to generate a unique symbol name, so just
 //   force it to be passed in.
-__attribute__((section(".llvm.compdb")))
+__attribute__((section(".clang.compdb")))
 extern const char __COMPDB_SYMNAME[] = "<<<COMPDB:" __COMPDB_ENTRY ">>>";
