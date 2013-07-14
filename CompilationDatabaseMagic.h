6562bb5c59335458afc804997116ba8c432c0785
Sean Silva <silvas@purdue.edu>
2013-07-14 01:55:44 -0700

    Make this file a bit prettier.

--- CompilationDatabaseMagic.h
+++ CompilationDatabaseMagic.h
@@ -1,6 +1,9 @@
-// __COMPDB_ENTRY: The contents of this compilation database entry.
-// Should be a string literal.
-// __COMPDB_SYMNAME: ATM I can't think of a way to generate a unique symbol
-// name, so just force the TU to provide it.
+// The following macros should be defined on entry to this file:
+// __COMPDB_ENTRY:
+//   The contents of this compilation database entry.  Should be a string
+//   literal.
+// __COMPDB_SYMNAME:
+//   ATM I can't think of a way to generate a unique symbol name, so just
+//   force it to be passed in.
 __attribute__((section(".llvm.compdb")))
 extern const char __COMPDB_SYMNAME[] = "<<<COMPDB:" __COMPDB_ENTRY ">>>";
